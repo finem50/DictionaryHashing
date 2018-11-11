ba9da3ec13cda2da34eb5b9caa6ab14ee9ecd007
Finem50 <Finem50@live.com>
2018-11-11 16:57:50 -0600

    Implemented values from input file to a vector. Values are returned from the vector and compared to values in the dictionary and tested for equality.

--- avlmain.cpp
+++ avlmain.cpp
@@ -3,8 +3,9 @@
 #include <fstream>
 #include <string>
 #include <locale>
+#include <vector>
 
-void input_build(std::string input);
+std::vector<std::string> input_build(std::string input);
 Dictionary dictionary_build(std::string dict);
 
 int main(){
@@ -12,32 +13,54 @@ int main(){
   //std::cout << "Please enter the name of a text file to spell check:" << std::endl;
   //std::cin >> in_file_name;
   std::string in_file_name = "inputSmall.txt";
+  std::string out_file_name;
   std::string dictionary_name = "dictSmall.txt";
+  std::ofstream out_s;
 
-  input_build(in_file_name);
-
+  std::vector<std::string> input = input_build(in_file_name);
   Dictionary avl_dict = dictionary_build(dictionary_name);
 
-//   if(avl_dict.FindEntry("impuse") == true){
-//
-//     std::cout << "It works!" << endl;
-//   }else
-//       std::cout << "It doesn't work..." << endl;
-//
+  std::cout << "Please enter the name of a file to output to:" << endl;
+  std::cin >> out_file_name;
+  out_s.open(out_file_name);
+
+  while(out_s.fail()){
+
+    out_s.clear();
+    std::cout << "Output file opening failed, please retry output file name." << std::endl;
+    std::cin >> out_file_name;
+    out_s.open(out_file_name.c_str());
+}
+
+  for(int i = 0; i < input.size(); i++){
+
+    if(avl_dict.FindEntry(input.at(i)) == true){
+
+      std::cout << input.at(i) << " is spelled correctly." << endl;
+    }else{
+
+      out_s << input.at(i) << endl;
+    }
+  }
+
+  // if(avl_dict.FindEntry("impuse") == true){
+  //
+  //   std::cout << "It works!" << endl;
+  // }else
+  //     std::cout << "It doesn't work..." << endl;
+
+
   return 0;
 }
 
 //Building input text file I/O
-void input_build(std::string input){
+std::vector<std::string> input_build(std::string input){
 
-  Dictionary check;
-  std::string out_file_name, str;
+  std::vector<std::string> check;
+  std::string str;
   std::ifstream in_s;
-  std::ofstream out_s;
   std::locale loc;
 
-  std::cout << "Please enter the name of a file to output to:" << std::endl;
-  std::cin >> out_file_name;
 
   in_s.open(input);
 
@@ -50,38 +73,27 @@ void input_build(std::string input){
     in_s.open(input.c_str());
   }
 
-  out_s.open(out_file_name);
-
-  while(out_s.fail()){
-
-    out_s.clear();
-    std::cout << "Output file opening failed, please retry output file name." << std::endl;
-    std::cin >> out_file_name;
-    in_s.open(out_file_name.c_str());
-  }
-
   while(std::getline(in_s, str)){
 
     for(std::string::size_type i = 0; i < str.length(); i++){
+
       str[i] = std::tolower(str[i], loc);
     }
 
-    check.AddEntry(str);
+    check.push_back(str);
   }
 
-  check.PrintSorted(out_s);
-
-  out_s.close();
   in_s.close();
+
+  return check;
 }
 
-//Building dictionary file I/O
+//Building dictionary AVL tree with file I/O
 Dictionary dictionary_build(std::string dict){
 
   Dictionary dictionary;
   std::string str;
   std::ifstream in_s;
-  std::ofstream out_s;
 
   in_s.open(dict);
 
@@ -98,7 +110,6 @@ Dictionary dictionary_build(std::string dict){
   }
 
   in_s.close();
-  out_s.close();
 
   return dictionary;
 }
